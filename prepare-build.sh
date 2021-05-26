09ba0f6cc40d77b97e2e8deb63309a23a40bdabc
Your Name <you@example.com>
2021-05-26 13:04:35 +0000

    change to relative paths

--- prepare-build.sh
+++ prepare-build.sh
@@ -3,32 +3,32 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-cd ~
+#cd ~
 
 sudo apt-get install python
 sudo apt-get install wiggle
 
 echo -e ${RED} -------- download bromite repo ${NC}
 git clone https://github.com/bromite/bromite
-cd ~/bromite
+cd bromite
 git fetch
 git pull
 
-VERSION=$( cat ~/bromite/build/RELEASE )
+VERSION=$( cat ./build/RELEASE )
 echo -e ${RED} -------- lastest version is: $VERSION ${NC}
 
-cd ~
+cd ..
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-PATH=~/gitui:~/chromium/src/third_party/llvm-build/Release+Asserts/bin:~/depot_tools/:/usr/local/go/bin:$PATH
+PATH=./chromium/src/third_party/llvm-build/Release+Asserts/bin:./depot_tools/:/usr/local/go/bin:$PATH
 
 echo -e ${RED} -------- download chromium repo ${NC}
-mkdir ~/chromium
-cd ~/chromium
+mkdir ./chromium
+cd ./chromium
 
 gclient root
 
-mkdir ~/chromium/src
-cd ~/chromium/src
+mkdir ./src
+cd ./src
 
 git init
 git remote add origin https://chromium.googlesource.com/chromium/src.git
@@ -37,22 +37,22 @@ git fetch --depth 2 https://chromium.googlesource.com/chromium/src.git +refs/tag
 git checkout $VERSION
 VERSION_SHA=$( git show-ref -s $VERSION | head -n1 )
 
-echo >~/chromium/.gclient "solutions = ["
-echo >>~/chromium/.gclient "  { \"name\"        : 'src',"
-echo >>~/chromium/.gclient "    \"url\"         : 'https://chromium.googlesource.com/chromium/src.git@$VERSION_SHA',"
-echo >>~/chromium/.gclient "    \"deps_file\"   : 'DEPS',"
-echo >>~/chromium/.gclient "    \"managed\"     : True,"
-echo >>~/chromium/.gclient "    \"custom_deps\" : {"
-echo >>~/chromium/.gclient "    },"
-echo >>~/chromium/.gclient "    \"custom_vars\": {},"
-echo >>~/chromium/.gclient "  },"
-echo >>~/chromium/.gclient "]"
-echo >>~/chromium/.gclient "target_os=['android']"
+echo >../.gclient "solutions = ["
+echo >>../.gclient "  { \"name\"        : 'src',"
+echo >>../.gclient "    \"url\"         : 'https://chromium.googlesource.com/chromium/src.git@$VERSION_SHA',"
+echo >>../.gclient "    \"deps_file\"   : 'DEPS',"
+echo >>../.gclient "    \"managed\"     : True,"
+echo >>../.gclient "    \"custom_deps\" : {"
+echo >>../.gclient "    },"
+echo >>../.gclient "    \"custom_vars\": {},"
+echo >>../.gclient "  },"
+echo >>../.gclient "]"
+echo >>../.gclient "target_os=['android']"
 
 #echo -e ${RED} -------- sync other chromium repos ${NC}
 
 #gclient metrics --opt-out
-git submodule foreach git config -f ~/chromium/src/.git/config submodule.$name.ignore all
+git submodule foreach git config -f ./.git/config submodule.$name.ignore all
 git config --add remote.origin.fetch '+refs/tags/*:refs/tags/*'
 #git config diff.ignoreSubmodules all
 
