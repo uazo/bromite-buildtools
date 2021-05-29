d494cd909eb3ae9e5811c5012bd005c8b91501d5
Your Name <you@example.com>
2021-05-29 17:48:33 +0000

    change ~ to $PWD

--- setup-goma-client.sh
+++ setup-goma-client.sh
@@ -8,19 +8,19 @@ cd ..
 PATH=$PWD/chromium/src/third_party/llvm-build/Release+Asserts/bin:$PWD/depot_tools/:/usr/local/go/bin:$PATH
 #------------------
 
-cipd install infra/goma/client/linux-amd64 -root ~/goma
+cipd install infra/goma/client/linux-amd64 -root $PWD/goma
 
-cat "nomatter" >~/.debug_auth_file
-cp bromite-buildtools/goma_auth.py ~/goma/
+cat "nomatter" >$PWD/.debug_auth_file
+cp bromite-buildtools/goma_auth.py $PWD/goma/
 
 export GOMA_SERVER_HOST=127.0.0.1
 export GOMA_SERVER_PORT=5050
 export GOMA_USE_SSL=false
-export GOMA_HTTP_AUTHORIZATION_FILE=~/.debug_auth_file
+export GOMA_HTTP_AUTHORIZATION_FILE=$PWD/.debug_auth_file
 export GOMA_HERMETIC=error
 export GOMA_USE_LOCAL=false
 export GOMA_FALLBACK=true
 export GOMA_ARBITRARY_TOOLCHAIN_SUPPORT=true
 
-~/goma/goma_ctl.py ensure_stop
-~/goma/goma_ctl.py ensure_start
+$PWD/goma/goma_ctl.py ensure_stop
+$PWD/goma/goma_ctl.py ensure_start
