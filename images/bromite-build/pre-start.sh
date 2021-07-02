8c08b7945137dee27548acc7f3a269d0c8a5d5f0
uazo <uazo@users.noreply.github.com>
2021-07-02 11:34:38 +0000

    add mtool

--- images/bromite-build/pre-start.sh
+++ images/bromite-build/pre-start.sh
@@ -7,7 +7,15 @@ WORKSPACE=/home/lg/working_dir
 
 PATH=$WORKSPACE/chromium/src/third_party/llvm-build/Release+Asserts/bin:$WORKSPACE/depot_tools/:/usr/local/go/bin:$PATH
 
-sudo apt-get install -y lsof libgoogle-glog-dev libprotobuf17 libgrpc++1
+sudo apt-get install -y lsof libgoogle-glog-dev libprotobuf17 libgrpc++1 parallel golang-go
+
+echo -e ${RED} -------- download mtool ${NC}
+git clone https://github.com/bromite/mtool
+cd mtool
+make
+
+echo -e ${RED} -------- download goma client ${NC}
+cd $WORKSPACE
 cipd install infra/goma/client/linux-amd64 -root $WORKSPACE/goma
 
 echo "nomatter" >$WORKSPACE/.debug_auth_file
