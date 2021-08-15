86525845879ae2510b0f73ba077a26968d578461
uazo <29201891+uazo@users.noreply.github.com>
2021-08-15 13:38:00 +0200

    move vpython root

--- images/bromite-build/pre-start.sh
+++ images/bromite-build/pre-start.sh
@@ -4,7 +4,6 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 WORKSPACE=/home/lg/working_dir
-
 PATH=$WORKSPACE/chromium/src/third_party/llvm-build/Release+Asserts/bin:$WORKSPACE/depot_tools/:/usr/local/go/bin:$PATH
 
 sudo apt-get install -y lsof libgoogle-glog-dev libprotobuf17 libgrpc++1 parallel golang-go
@@ -13,6 +12,9 @@ echo -e ${RED} -------- download mtool ${NC}
 git clone https://github.com/bromite/mtool
 cd mtool
 make
+cd ..
+
+mkdir $CIPD_CACHE_DIR
 
 echo -e ${RED} -------- download goma client ${NC}
 cd $WORKSPACE
@@ -22,11 +24,6 @@ echo "nomatter" >$WORKSPACE/.debug_auth_file
 sudo cp $WORKSPACE/goma_auth.py $WORKSPACE/goma/
 
 echo -e ${RED} -------- prepare vpython virtual environment
-sudo mkdir -p /github/home/.vpython_cipd_cache
-sudo chown lg /github/home/.vpython_cipd_cache
-
-sudo mkdir -p /github/home/.vpython-root
-sudo chown lg /github/home/.vpython-root
-
+rm -rf /tmp/vpython*
 cd $WORKSPACE/chromium/src
-vpython -vpython-spec .vpython -vpython-log-level debug -vpython-tool install
+vpython -vpython-spec .vpython -vpython-root . -vpython-log-level debug -vpython-tool install
