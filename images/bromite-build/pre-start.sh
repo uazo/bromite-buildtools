3b0e563153bafd007ad17adeb85a82116c98a50a
uazo <29201891+uazo@users.noreply.github.com>
2021-08-17 12:01:28 +0200

    fix destination dir

--- images/bromite-build/pre-start.sh
+++ images/bromite-build/pre-start.sh
@@ -29,4 +29,4 @@ cd $WORKSPACE/chromium/src
 vpython -vpython-spec .vpython -vpython-root . -vpython-log-level debug -vpython-tool install
 
 echo -e ${RED} -------- download x86_64 android image ${NC}
-echo -e "\$ParanoidMode CheckIntegrity\n\nchromium/third_party/android_sdk/public/avds/android-31/google_apis/x86_64 Ur_zl6_BRKRkf_9X3SMZ3eH2auoOyJ2kLslpTZZwi3gC" | .cipd_client ensure -ensure-file - -root chromium/src/.android
+echo -e "\$ParanoidMode CheckIntegrity\n\nchromium/third_party/android_sdk/public/avds/android-31/google_apis/x86_64 Ur_zl6_BRKRkf_9X3SMZ3eH2auoOyJ2kLslpTZZwi3gC" | .cipd_client ensure -ensure-file - -root $WORKSPACE/chromium/src/.android
