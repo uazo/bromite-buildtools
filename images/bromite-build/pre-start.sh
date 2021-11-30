ac5100b048fc9a3ff3951cb634aa8a8c2ce74c35
uazo <29201891+uazo@users.noreply.github.com>
2021-11-30 15:25:29 +0100

    Add kythe resources

--- images/bromite-build/pre-start.sh
+++ images/bromite-build/pre-start.sh
@@ -33,3 +33,9 @@ vpython3 -vpython-spec .vpython3 -vpython-root $VPYTHON_VIRTUALENV_ROOT -vpython
 echo -e ${RED} -------- download x86_64 android image ${NC}
 echo -e "\$ParanoidMode CheckIntegrity\n\nchromium/third_party/android_sdk/public/avds/android-31/google_apis/x86_64 Ur_zl6_BRKRkf_9X3SMZ3eH2auoOyJ2kLslpTZZwi3gC" | .cipd_client ensure -ensure-file - -root $WORKSPACE/chromium/src/.android
 echo -e "\$ParanoidMode CheckIntegrity\n\nchromium/third_party/android_sdk/public/emulator gMHhUuoQRKfxr-MBn3fNNXZtkAVXtOwMwT7kfx8jkIgC\nchromium/third_party/android_sdk/public/system-images/android-31/google_apis/x86_64 R6Jh5_P21Euu-kdb11zcNjdJKN4vV1mdQTb8t4gph4IC" | .cipd_client ensure -ensure-file - -root $WORKSPACE/chromium/src/.emulator_sdk
+
+echo -e ${RED} -------- download kythe resources ${NC}
+wget https://chromium.googlesource.com/chromium/tools/build/+/main/recipes/recipe_modules/codesearch/resources/add_kythe_metadata.py?format=TEXT -O ~/add_kythe_metadata.py.base64
+base64 -d ~/add_kythe_metadata.py.base64 >~/add_kythe_metadata.py
+echo -e "infra/tools/package_index/linux-amd64 latest" | .cipd_client ensure -ensure-file - -root ~/package_index/latest
+python tools/clang/scripts/update.py --package=translation_unit
