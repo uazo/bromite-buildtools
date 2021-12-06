8de587281eed7744dfda387ccf0e01f684c3265c
uazo <29201891+uazo@users.noreply.github.com>
2021-12-06 14:31:09 +0100

    reapply Automated-domain-substitution.patch

--- images/bromite-source/apply-bromite-patches.sh
+++ images/bromite-source/apply-bromite-patches.sh
@@ -10,10 +10,10 @@ cd chromium/src
 echo -e ${RED} ------- apply patchs ${NC}
 for file in $(cat ../../bromite/build/bromite_patches_list.txt) ; do
 
-	if [[ "$file" == *"Automated-domain-substitution"* ]]; then
-		echo -e ${RED} " -> Excluding $file" ${NC}
-		continue
-	fi
+	#if [[ "$file" == *"Automated-domain-substitution"* ]]; then
+	#	echo -e ${RED} " -> Excluding $file" ${NC}
+	#	continue
+	#fi
 	
 	echo -e ${RED} " -> Apply $file" ${NC}
 
