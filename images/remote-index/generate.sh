095f1c88ec6baa4f90a1d2a301eca18d823b0b18
uazo <29201891+uazo@users.noreply.github.com>
2022-01-12 09:23:42 +0100

    Create generate.sh

---
+++ images/remote-index/generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -z "${DOCKER_SOCK}" ]]; then
+   echo "Please set DOCKER_SOCK env variable"
+   exit 1
+fi
+
+if [[ -z "${DEV_CONTAINER}" ]]; then
+   echo "Please set DEV_CONTAINER env variable"
+   exit 1
+fi
+
+sudo docker -H $DOCKER_SOCK cp $DEV_CONTAINER:/home/lg/working_dir/chromium/src/out/bromite/bromite.idx .
+sudo docker -H $DOCKER_SOCK cp $DEV_CONTAINER:/home/lg/working_dir/bromite/build/RELEASE .
+
+INDEX_VERSION=$(cat RELEASE)
+DOCKER_BUILDKIT=1 docker build -t uazo/bromite-remote-index:$INDEX_VERSION \
+                --progress plain \
+                --no-cache \
+                .
