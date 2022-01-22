43261db144f6061e3140c59caaec266aaa60fa27
uazo <29201891+uazo@users.noreply.github.com>
2022-01-22 09:18:35 +0100

    fix remote index tag

--- images/remote-index/generate.sh
+++ images/remote-index/generate.sh
@@ -13,7 +13,7 @@ fi
 sudo docker -H $DOCKER_SOCK cp $DEV_CONTAINER:/home/lg/working_dir/chromium/src/out/bromite/bromite.idx .
 sudo docker -H $DOCKER_SOCK cp $DEV_CONTAINER:/home/lg/working_dir/bromite/build/RELEASE .
 
-INDEX_VERSION=$(cat RELEASE)
+#INDEX_VERSION=$(cat RELEASE)
 DOCKER_BUILDKIT=1 docker build -t uazo/bromite-remote-index:$INDEX_VERSION \
                 --progress plain \
                 --no-cache \
