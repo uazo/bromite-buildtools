50441d00bc719c08a02516518a066eee2b9a8560
uazo <29201891+uazo@users.noreply.github.com>
2022-09-07 11:11:24 +0200

    improved replacement

--- export-single-patch.sh
+++ export-single-patch.sh
@@ -16,16 +16,29 @@ fi
 git -C ~/chromium/src/ format-patch -1 --keep-subject --stdout --full-index --zero-commit --no-signature $patch >~/bromite/build/patches-new/$PATCH_FILE
 echo "   exported"
 
+CHANGE_REF=""
 while read line; do
-	#echo $line
-	if [[ "$line" == index* ]]; then
-		next_line=$(grep -A1 "${line}" ~/bromite/build/patches-new/$PATCH_FILE | tail -n 1 )
-		if [[ "$next_line" != "GIT binary patch" ]]; then
-			sed -i "/^$line/d" ~/bromite/build/patches-new/$PATCH_FILE
+	for i in {1..5}
+	do
+		if [[ "$line" == index* ]]; then
+			read next_line
+			if [[ "$next_line" != "GIT binary patch" ]]; then
+				CHANGE_REF=${CHANGE_REF}"/^${line}/d;"
+				break
+			else
+				line=$next_line
+				continue
+			fi
+		else
+			break
 		fi
-	fi
+	done
 done <~/bromite/build/patches-new/$PATCH_FILE
 
+if [ "$CHANGE_REF" ]
+then
+	sed -i "$CHANGE_REF" ~/bromite/build/patches-new/$PATCH_FILE
+fi
 sed -i '/^From 0000000000000000000000000000000000000000/d' ~/bromite/build/patches-new/$PATCH_FILE
 sed -i '/^FILE:/d' ~/bromite/build/patches-new/$PATCH_FILE
 sed -i '/^ mode change/d' ~/bromite/build/patches-new/$PATCH_FILE
